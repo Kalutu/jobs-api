d740004fc220e428ab9fcdf2a7823b4f2531d035
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-22 08:38:20 +0300

    Set up the models

---
+++ models/Job.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+
+const JobSchema = new mongoose.Schema({
+    company: {
+        type: String,
+        required: [true, 'Please provide company name'],
+        maxlength: 50
+    },
+    position: {
+        type: String,
+        required: [true, 'Please provide position'],
+        maxlength: 100
+    },
+    status: {
+        type: String,
+        enum: ['inteview', 'declined', 'pending'],
+        default: 'pending'
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please provide user']
+    }
+},{timestamps: true})
+
+module.exports = mongoose.model('Job', JobSchema)
